b382a1e4abd2e17dffe8341be41d370e2d9bcf94
Caleb Littlejohn <calebl@mit.edu>
2019-12-06 17:52:42 -0500

    Minor fixes

--- web/settings.py
+++ web/settings.py
@@ -32,7 +32,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'clittlejohn268@gmail.com'
-EMAIL_HOST_PASSWORD = 'newtab15'
+EMAIL_HOST_PASSWORD = '********'
 
 LOGOUT_REDIRECT_URL = '/'
 
