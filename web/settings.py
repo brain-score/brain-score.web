ed44446624d2ad1f4a7eb36c8952f09b3f258f5d
franzigeiger <franzigeiger94@gmail.com>
2020-07-31 12:23:03 +0200

    default lower case

--- web/settings.py
+++ web/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     # for working local:
-    'DEFAULT': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
