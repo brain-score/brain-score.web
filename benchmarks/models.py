4633632100af96974bebe4404afdb0a314454a40
Martin Schrimpf <m4rtinsch@gmail.com>
2021-07-08 17:54:03 -0400

    fix model natural key without owner
    use name instead of identifier

--- benchmarks/models.py
+++ benchmarks/models.py
@@ -172,8 +172,8 @@ class Model(models.Model):
         return self.name, self.owner
 
     class ModelManager(models.Manager):
-        def get_by_natural_key(self, identifier, owner=None):
-            kwargs = dict(identifier=identifier)
+        def get_by_natural_key(self, name, owner=None):
+            kwargs = dict(name=name)
             if owner is not None:  # if owner is passed, explicitly use it to link, otherwise try without
                 kwargs['owner'] = owner
             return self.get(**kwargs)
