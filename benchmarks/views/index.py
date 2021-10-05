0fb40a72612a8530e2c35ff3b91658abb400eb67
Michael Ferguson <mef8dd@virginia.edu>
2021-10-05 11:00:57 -0400

    Cleaned up console log code, got rid of unneeded fields

--- benchmarks/views/index.py
+++ benchmarks/views/index.py
@@ -230,7 +230,7 @@ def _collect_models(benchmarks, user=None):
         'id', 'name',
         'reference_identifier', 'reference_link',
         'user', 'public',
-        'rank', 'scores', 'build_status', 'console_log', 'timestamp', 'submitter', 'submission_id'])
+        'rank', 'scores', 'build_status', 'timestamp', 'submitter', 'submission_id'])
     ScoreDisplay = namedtuple('ScoreDiplay', field_names=[
         'benchmark', 'benchmark_depth', 'order', 'score_raw', 'score_ceiled', 'error', 'color'])
     # - prepare "no score" objects for when a model-benchmark score is missing
@@ -282,7 +282,6 @@ def _collect_models(benchmarks, user=None):
             _logger.warning(f"Model {model_id} not found in model_ranks")
             rank = max(model_ranks['rank']) + 1
         reference_identifier = f"{meta.reference.author} et al., {meta.reference.year}" if meta.reference else None
-        console_log = "This is the model's console log, to be replaced by the log itself."
         timestamp = meta.submission.timestamp
         submitter = meta.submission.submitter
         submission_id = meta.submission.id
@@ -291,7 +290,7 @@ def _collect_models(benchmarks, user=None):
             name=meta.name,
             reference_identifier=reference_identifier, reference_link=meta.reference.url if meta.reference else None,
             user=meta.owner, public=meta.public,
-            scores=model_scores, rank=rank, build_status=meta.submission.status, console_log=console_log,
+            scores=model_scores, rank=rank, build_status=meta.submission.status,
             timestamp=timestamp, submitter=submitter, submission_id=submission_id)
         data.append(model_row)
     data = list(sorted(data, key=lambda model_row: model_row.rank))
