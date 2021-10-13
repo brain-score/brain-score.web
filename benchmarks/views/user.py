brain-score/brain-score.web

commit 5c5899ec438d1b13666dadbc5a278ec6d2989a1b

Model dashboard (#86)
* draft model dashboard, re-use components

* per-score rank; region-layer ordering; link from table

* bigger images with overflow wrap

* add function to sample benchmark images for website dashboard

previously https://github.com/brain-score/brain-score/pull/300

* show visual-degrees examples; brainmodel info after scores

* rename utils->content_utils

* update bulma

* simplify bulma import

* include title classes and div blocks

* use box and tag; marginless visual-degrees images

* exclude content_utils from setup

* update bulma

* redesign table with bulma

* clean up visual degree samples

* test benchmarks module only

* speed up query efficiency

* add visual_degrees images

* fix visual_degrees rescaling

* store visual degrees in model; per-user model retrieval

* store benchmark meta in database

* show reference, tag, and layers only when available

* only show scores that were run

* get rid of out-dated css

* re-work info boxes

* put benchmark meta inside div

* fix building comparison_data

* rename to compare instead of analysis

* add visual_degrees to models fixture

* raise 404 for no user access

* add benchmark reference

* add migration for visual_degrees and benchmark meta

* only show ceiling tooltip when ceiling exists

* increase table margin to show version on FreemanZiemba

* resize benchmark image samples to max height

* open model page in same tab

* rename benchmark_specifier -> versioned_benchmark_identifier

* add benchmark sample images

* allow null for visual_degrees

used during model creation, before instantiating the model object from the pool

* deal with null visual degrees in view

* add expandable bibtex string to model view

* show score overview dashboard

* check for existence of d3 compare container

avoids console errors

* collapse models' benchmark scores

benchmarks/templates/benchmarks/submissions.html
@@ -7,23 +7,31 @@
         <div class="columns">
             <div class="column is-half">
                 <h4>Pick benchmarks</h4>
-                {% for benchmark in benchmarks|no_children:models %}
+                {% for short_name, benchmark_type_id in submittable_benchmarks.items %}
                     <div id=benchmarkGroup" class="field">
-                        <input class="is-checkradio is-block benchmark_checker"
-                               name='benchmarks_{{ benchmark.identifier }}' onclick="enableButtonBench()"
-                               value="{{ benchmark.identifier }}" id="{{ benchmark.short_name }}" type="checkbox">
-                        <label for="{{ benchmark.short_name }}">{{ benchmark.short_name }}</label>
+                        <input class="checkbox is-checkradio is-block benchmark_checker" type="checkbox"
+                               onclick="enableButtonBench()"
+                               name='benchmark_selection'
+                               value="{{ benchmark_type_id }}"
+                               id="benchmark-{{ benchmark_type_id }}">
+                        <label class="checkbox" for="benchmark-{{ benchmark_type_id }}">
+                            {{ short_name }}
+                        </label>
                     </div>
                 {% endfor %}
             </div>
             <div class="column">
                 <h4>Pick models</h4>
-                {% for model in models|no_benchmark:benchmarks %}
+                {% for model in models %}
                     <div id="modelsGroup" class="field">
-                        <input class="is-checkradio is-block model_checker" name='models_{{ model.name }}'
-                               onclick="enableButtonModel()" value="{{ model.id }}" id="{{ model.name }}"
-                               type="checkbox">
-                        <label for="{{ model.name }}">{{ model.name }}</label>
+                        <input class="checkbox is-checkradio is-block model_checker" type="checkbox"
+                               onclick="enableButtonModel()"
+                               name='model_selection'
+                               value="{{ model.id }}"
+                               id="model-{{ model.id }}">
+                        <label class="checkbox" for="model-{{ model.id }}">
+                            {{ model.name }}
+                        </label>
                     </div>
                 {% endfor %}
             </div>

benchmarks/views/user.py
@@ -106,7 +106,7 @@ class Logout(View):
 
 class Upload(View):
     def get(self, request):
-        if str(request.user) == "AnonymousUser":
+        if request.user.is_anonymous:
             return HttpResponseRedirect('../profile/')
         form = UploadFileForm()
         return render(request, 'benchmarks/upload.html', {'form': form})
@@ -154,13 +154,14 @@ def resubmit(request):
         model_ids = []
         benchmarks = []
         for key, value in request.POST.items():
-            if 'models_' in key:
+            if key == 'model_selection':
+                # value in this case is the model id
                 model = Model.objects.get(id=value)  # get model instance uniquely referenced with the id
                 verify_user_model_access(user=request.user, model=model)
                 model_ids.append(model.id)
-            if 'benchmarks_' in key:
-                # benchmark identifiers are versioned, which we have to remove for submitting to jenkins
-                benchmarks.append(value.split('_v')[0])
+            if key == 'benchmark_selection':
+                # value in this case is the benchmark_type_id
+                benchmarks.append(value)
         if len(model_ids) > 0 and len(benchmarks) > 0:
             json_info = {
                 "user_id": request.user.id,
@@ -203,7 +204,7 @@ class DisplayName(View):
 
 class Profile(View):
     def get(self, request):
-        if str(request.user) == "AnonymousUser":
+        if request.user.is_anonymous:
             return render(request, 'benchmarks/login.html', {'form': LoginForm})
         else:
             context = get_context(request.user)
@@ -314,7 +315,7 @@ class PublicAjax(View):
 
 def verify_user_model_access(user, model):
     # make sure user is allowed to perform this operation: either model owner or superuser
-    if not (user.is_superuser or model.owner == user.id):
+    if not (user.is_superuser or model.owner == user):
         raise PermissionDenied(f"User {user} is not allowed access to model {model}")
 
 

commit 80c554a220d94a7c287d01786395d6bc276303cb

fix re-submission following incorrect key use; add error page
broken in https://github.com/brain-score/brain-score.web/pull/86

benchmarks/templates/benchmarks/submission_error.html
@@ -1,0 +1,16 @@
+{% extends 'benchmarks/base.html' %}
+
+{% block content %}
+    <section class="container center">
+        <article class="message is-danger">
+            <div class="message-body">
+                Submission of models failed.
+            </div>
+        </article>
+        <p>{{ error }}</p>
+        <br/>
+        <p>To return to your profile page, click <a href="../profile">here</a>.</p>
+        <br/>
+        <p>To submit another model, click <a href="../upload">here</a></p>
+    </section>
+{% endblock %}

benchmarks/templates/benchmarks/submissions.html
@@ -11,7 +11,8 @@
                     <div id=benchmarkGroup" class="field">
                         <input class="checkbox is-checkradio is-block benchmark_checker" type="checkbox"
                                onclick="enableButtonBench()"
-                               name='benchmark_selection'
+                                {# add unique id to form name to allow simultaneous submission of multiple benchmarks #}
+                               name='benchmark_selection_{{ benchmark_type_id }}'
                                value="{{ benchmark_type_id }}"
                                id="benchmark-{{ benchmark_type_id }}">
                         <label class="checkbox" for="benchmark-{{ benchmark_type_id }}">
@@ -26,7 +27,8 @@
                     <div id="modelsGroup" class="field">
                         <input class="checkbox is-checkradio is-block model_checker" type="checkbox"
                                onclick="enableButtonModel()"
-                               name='model_selection'
+                               {# add unique id to form name to allow simultaneous submission of multiple models #}
+                               name='model_selection_{{ model.id }}'
                                value="{{ model.id }}"
                                id="model-{{ model.id }}">
                         <label class="checkbox" for="model-{{ model.id }}">

benchmarks/views/user.py
@@ -150,51 +150,50 @@ class Upload(View):
 
 
 def resubmit(request):
-    if request.method == 'POST':
-        _logger.debug(f"request user: {request.user.get_full_name()}")
-        model_ids = []
-        benchmarks = []
-        for key, value in request.POST.items():
-            if key == 'model_selection':
-                # value in this case is the model id
-                model = Model.objects.get(id=value)  # get model instance uniquely referenced with the id
-                verify_user_model_access(user=request.user, model=model)
-                model_ids.append(model.id)
-            if 'benchmarks_' in key:
-                # benchmark identifiers (benchmark_type_id) are versioned,
-                # which we have to remove for submitting to jenkins
-                identifier, version = split_identifier_version(value)
-                benchmarks.append(identifier)
-        if len(model_ids) > 0 and len(benchmarks) > 0:
-            json_info = {
-                "user_id": request.user.id,
-                "model_ids": model_ids,
-            }
-            with open('result.json', 'w') as fp:
-                json.dump(json_info, fp)
+    assert request.method == 'POST'
 
-            # submit to jenkins
-            jenkins_url = "http://braintree.mit.edu:8080"
-            auth = get_secret("brainscore-website_jenkins_access")
-            auth = (auth['user'], auth['password'])
-            job_name = "run_benchmarks"
-            s = ' '
-            benchmark_string = s.join(benchmarks)
-            request_url = f"{jenkins_url}/job/{job_name}/buildWithParameters" \
-                          f"?TOKEN=trigger2scoreAmodel" \
-                          f"&email={request.user.get_full_name()}" \
-                          f"&benchmarks={benchmark_string}"
-            _logger.debug(f"request_url: {request_url}")
-            params = {'submission.config': open('result.json', 'rb')}
-            response = requests.post(request_url, files=params, auth=auth)
-            _logger.debug(f"response: {response}")
+    _logger.debug(f"request user: {request.user.get_full_name()}")
+    model_ids = []
+    benchmarks = []
+    for key, value in request.POST.items():
+        if key.startswith('model_selection_'):
+            # value in this case is the model id
+            model = Model.objects.get(id=value)  # get model instance uniquely referenced with the id
+            verify_user_model_access(user=request.user, model=model)
+            model_ids.append(model.id)
+        elif key.startswith('benchmark_selection_'):
+            # value is benchmark_type_id (un-versioned)
+            benchmarks.append(value)
+    if len(model_ids) > 0 and len(benchmarks) > 0:
+        json_info = {
+            "user_id": request.user.id,
+            "model_ids": model_ids,
+        }
+        with open('result.json', 'w') as fp:
+            json.dump(json_info, fp)
 
-            # update frontend
-            response.raise_for_status()
-            _logger.debug("Job triggered successfully")
-            return render(request, 'benchmarks/success.html')
-        else:
-            return render(request, 'benchmark/')
+        # submit to jenkins
+        jenkins_url = "http://braintree.mit.edu:8080"
+        auth = get_secret("brainscore-website_jenkins_access")
+        auth = (auth['user'], auth['password'])
+        job_name = "run_benchmarks"
+        s = ' '
+        benchmark_string = s.join(benchmarks)
+        request_url = f"{jenkins_url}/job/{job_name}/buildWithParameters" \
+                      f"?TOKEN=trigger2scoreAmodel" \
+                      f"&email={request.user.get_full_name()}" \
+                      f"&benchmarks={benchmark_string}"
+        _logger.debug(f"request_url: {request_url}")
+        params = {'submission.config': open('result.json', 'rb')}
+        response = requests.post(request_url, files=params, auth=auth)
+        _logger.debug(f"response: {response}")
+
+        # update frontend
+        response.raise_for_status()
+        _logger.debug("Job triggered successfully")
+        return render(request, 'benchmarks/success.html')
+    else:
+        return render(request, 'benchmarks/submission_error.html', {'error': "No model ids and benchmarks found"})
 
 
 class DisplayName(View):