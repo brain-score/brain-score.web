0cf44353bc1d76222738ca2fc75de77e2a817f87
Michael Ferguson <mef8dd@virginia.edu>
2021-11-24 16:47:39 -0500

    MVP for competition site

--- benchmarks/views/competition.py
+++ benchmarks/views/competition.py
@@ -1,14 +1,9 @@
 from .index import get_context
 from django.shortcuts import render
-
-# from ..models import CompetitionSubmission
-from ..models import Submission
+import pdb
 
 
 def view(request):
     context = get_context()
-
-    # change to commented out line once fixtures are put into place
-    # context['models'] = [model for model in context['models'] if model.competition == 'cosyne2022']
-    context['models'] = [model for model in context['models'] if model.submission_id >= 100]
+    context['models'] = [model for model in context['models'] if model.competition == 'cosyne2022']
     return render(request, 'benchmarks/competition.html', context)
