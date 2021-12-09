3b738b24d9355ad7482076a0eda30c7b604e71e2
Martin Schrimpf <m4rtinsch@gmail.com>
2021-12-09 00:22:53 -0500

    append trailing slash to competition url

--- benchmarks/tests/test_views.py
+++ benchmarks/tests/test_views.py
@@ -25,11 +25,11 @@ class TestCompetitionTable(TestCase):
     fixtures = ALL_FIXTURES
 
     def test_no_errors(self):
-        resp = self.client.get("http://localhost:8000/competition")
+        resp = self.client.get("http://localhost:8000/competition/")
         self.assertEqual(resp.status_code, 200)
 
     def test_num_rows(self):
-        resp = self.client.get("http://localhost:8000/competition")
+        resp = self.client.get("http://localhost:8000/competition/")
         content = resp.content.decode('utf-8')
         num_rows = content.count("<tr>")
         self.assertEqual(num_rows, 1 + 7)
