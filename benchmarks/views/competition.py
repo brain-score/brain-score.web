ebf6b6cd0f6424465b921f809833bdc0671f2d59
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2022-01-27 13:44:00 -0500

    group competition models by user (#130)
    * group competition models by user
    
    * only show models scoring best per track in the table
    * add expand/collapse to show secondary (not top) models per user
    * also small cosmetic code fixes
    
    * update fixtures for different owners per model; update test to account for user grouping
    
    * fix using user instead of submitter
    
    * fix counting of rows with classes; test secondary models

--- benchmarks/views/competition.py
+++ benchmarks/views/competition.py
@@ -1,9 +1,12 @@
+import random
+import string
 from collections import namedtuple
+from operator import itemgetter
 
+from django.shortcuts import render
 from numpy.random.mtrand import RandomState
 
 from .index import get_context
-from django.shortcuts import render
 
 NUM_STIMULI_SAMPLES = 150
 
@@ -13,13 +16,14 @@ def view(request):
     for key, selection_filter in [
         ('V1', lambda benchmarks: benchmarks.exclude(identifier__in=['V2', 'V4', 'IT', 'behavior'])),
         ('behavior', lambda benchmarks: benchmarks.exclude(identifier__in=['V1', 'V2', 'V4', 'IT'])),
-        ('average', lambda benchmarks: benchmarks),  # average last to have the full set of adjacent variables
+        ('average', lambda benchmarks: benchmarks),  # average last to have full set of adjacent variables in context
     ]:
         # brain benchmarks only, ignore temporal benchmark
         base_filter = lambda benchmarks: benchmarks.exclude(identifier__in=['engineering', 'dicarlo.Kar2019-ost'])
         benchmark_filter = lambda benchmarks: selection_filter(base_filter(benchmarks))
         key_context = get_context(benchmark_filter=benchmark_filter,
                                   model_filter=dict(model__competition='cosyne2022'), show_public=True)
+        group_by_user(key_context)
         key_context[f"benchmarks_{key}"] = key_context['benchmarks']
         key_context[f"models_{key}"] = key_context['models']
         del key_context['benchmarks'], key_context['models']
@@ -31,6 +35,40 @@ def view(request):
                            ]
     context['stimuli_samples'] = create_stimuli_samples(benchmark_instances, num_samples=NUM_STIMULI_SAMPLES)
     return render(request, 'benchmarks/competition.html', context)
+
+
+def group_by_user(context):
+    models = context['models']
+    # group
+    submitters = [m.user.email +
+                  # append random string to superuser to treat baselines as coming from separate users
+                  (''.join(random.choices(string.ascii_uppercase, k=3)) if m.user.is_superuser else '')
+                  for m in models]
+    unique_submitters, unique_indices = unique_in_order(submitters, return_index=True)
+    top_models = itemgetter(*unique_indices)(models)
+    # re-rank
+    reranked_models = []
+    for rank, top_model in enumerate(top_models, start=1):
+        top_model = top_model._replace(rank=rank)
+        # find secondary models that also belong to this user
+        secondary_models = [model for model in models
+                            if model.user == top_model.user
+                            and model.id != top_model.id
+                            and not model.user.is_superuser]
+        # add count to model row
+        model_dict = top_model._asdict()
+        TopModel = namedtuple('TopModel', list(model_dict.keys()) + ['num_secondary_models'])
+        top_model = TopModel(*(list(model_dict.values()) + [len(secondary_models)]))
+        reranked_models.append(top_model)
+        # append secondary models
+        for secondary_model in secondary_models:
+            secondary_model = secondary_model._replace(rank='')
+            model_dict = secondary_model._asdict()
+            SecondaryModel = namedtuple('SecondaryModel', list(model_dict.keys()) + ['primary_model_id'])
+            secondary_model = SecondaryModel(*(list(model_dict.values()) + [top_model.id]))
+            reranked_models.append(secondary_model)
+
+    context['models'] = reranked_models
 
 
 def create_stimuli_samples(benchmark_instances, num_samples, available_sample_per_benchmark=30):
@@ -53,3 +91,15 @@ def create_stimuli_samples(benchmark_instances, num_samples, available_sample_pe
     samples = list(sorted(samples))  # make order deterministic
     random.shuffle(samples)  # reshuffle
     return samples
+
+
+def unique_in_order(sequence, return_index=False):
+    """
+    returns the unique items in the sequence, in the order that they occur in the sequence
+    """
+    # from https://stackoverflow.com/a/480227/2225200
+    seen = set()
+    seen_add = seen.add
+    unique = [(x, index) for index, x in enumerate(sequence) if not (x in seen or seen_add(x))]
+    unique, indices = zip(*unique)
+    return (unique, indices) if return_index else unique
