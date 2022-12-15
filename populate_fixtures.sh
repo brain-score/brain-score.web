340d3dca5d581860ef243a59315927930ab635bd
Mike Ferguson <mef8dd@virginia.edu>
2022-12-15 10:35:13 -0500

    updated fixtures

--- populate_fixtures.sh
+++ populate_fixtures.sh
@@ -1,7 +1,6 @@
 echo "Migrating new db changes..."
 python manage.py makemigrations
 python manage.py migrate
-
 echo "Populating Fixtures now..."
 python manage.py loaddata benchmarks/fixtures/fixture-benchmarkreferences.json
 python manage.py loaddata benchmarks/fixtures/fixture-benchmarktypes.json
@@ -12,4 +11,11 @@ python manage.py loaddata benchmarks/fixtures/fixture-submissions.json
 python manage.py loaddata benchmarks/fixtures/fixture-modelreferences.json
 python manage.py loaddata benchmarks/fixtures/fixture-models.json
 python manage.py loaddata benchmarks/fixtures/fixture-scores.json
+echo "Installing language fixtures..."
+python manage.py loaddata benchmarks/fixtures/fixture-benchmarktypes-language.json
+python manage.py loaddata benchmarks/fixtures/fixture-benchmarkmeta-language.json
+python manage.py loaddata benchmarks/fixtures/fixture-benchmarkinstances-language.json
+python manage.py loaddata benchmarks/fixtures/fixture-users-language.json
+python manage.py loaddata benchmarks/fixtures/fixture-models-language.json
+python manage.py loaddata benchmarks/fixtures/fixture-scores-language.json
 echo "Done. Check db.sqlite file."
