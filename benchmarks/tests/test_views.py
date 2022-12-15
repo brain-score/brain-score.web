3d39d036049c0067062fe07341a40fd18ae3d9d5
Mike Ferguson <mef8dd@virginia.edu>
2022-12-15 10:57:35 -0500

    Added more tests for language

--- benchmarks/tests/test_views.py
+++ benchmarks/tests/test_views.py
@@ -50,12 +50,12 @@ class TestModel(TestCase):
     fixtures = ALL_FIXTURES
 
     def test_public_model(self):
-        resp = self.client.get("http://localhost:8000/model/1")
+        resp = self.client.get("http://localhost:8000/model/vision/1")
         self.assertEqual(resp.status_code, 200)
 
     def test_non_public_model(self):
         # test no returns 200 after competition update (model publicity schema is changed)
-        resp = self.client.get("http://localhost:8000/model/2")
+        resp = self.client.get("http://localhost:8000/model/vision/2")
         self.assertEqual(resp.status_code, 200)
 
 
@@ -71,3 +71,27 @@ class TestIdentifierVersionSplit(TestCase):
         identifier, version = split_identifier_version(versioned_benchmark_identifier)
         self.assertEqual(identifier, 'dicarlo.Marques2020_Ringach2002-circular_variance')
         self.assertEqual(version, '1')
+
+
+class TestLanguage(TestCase):
+    fixtures = ALL_FIXTURES
+
+    def test_public_model_language(self):
+        resp = self.client.get("http://localhost:8000/model/language/92")
+        self.assertEqual(resp.status_code, 200)
+
+    def test_non_public_model_language(self):
+        # test no returns 200 after competition update (model publicity schema is changed)
+        resp = self.client.get("http://localhost:8000/model/language/89")
+        self.assertEqual(resp.status_code, 200)
+
+    # ensures language homepage exists
+    def test_language_leaderboard(self):
+        resp = self.client.get("http://localhost:8000/language")
+        self.assertEqual(resp.status_code, 200)
+
+    def test_num_lang_rows(self):
+        resp = self.client.get("http://localhost:8000/language")
+        content = resp.content.decode('utf-8')
+        num_rows = content.count("<tr")
+        self.assertEqual(num_rows, 1 + 9)
