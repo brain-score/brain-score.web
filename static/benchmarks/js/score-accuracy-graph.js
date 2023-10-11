212b3d8cab043c7ff7e577d62ba1bfa8eb24a449
SEBASTIAN FAIRCHILD <fairchild.seb@gmail.com>
2023-10-10 20:34:00 -0400

    Updated leaderboard view

---
+++ static/benchmarks/js/score-accuracy-graph.js
@@ -0,0 +1,54 @@
+$(document).ready(() => {
+  const leaderboardTableComponent = document.querySelector('.leaderboard-table-component');
+  const leaderboardTotals = document.querySelector('.leaderboard-totals');
+  const graphContainer = document.querySelector('.leaderboard-graph-container');
+
+  // https://www.chartjs.org/docs/latest/
+  new Chart(
+    document.getElementById('scores_time_graph'),
+    {
+      type: 'line',
+      data: {
+        datasets: [{
+          label: 'Top Score',
+          data: score_accuracy_chart.map(row => ({
+            x: row.day,
+            y: row.score,
+            label: [row.model.name, row.score]
+          }))
+        }]
+      },
+      options: {
+        normalized: true,
+        maintainAspectRatio: false,
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: (context) => context.raw.label
+            }
+          }
+        }
+      }
+    }
+  );
+
+  // let the page render before setting the dimensions
+  setTimeout(setGraphContainerDimensions);
+
+  window.addEventListener("resize", setGraphWidth);
+
+  function setGraphContainerDimensions() {
+    setGraphWidth();
+    setGraphHeight();
+  };
+
+  function setGraphWidth() {
+    graphContainer.style.width = (leaderboardTableComponent.offsetWidth - leaderboardTotals.offsetWidth) + 'px';      
+  };
+
+  function setGraphHeight() {
+    // the height of both totals cards plus 24px padding;
+    const rowHeight = (document.querySelector('.leaderboard-totals-card').offsetHeight * 2) + 24;
+    graphContainer.style.height = rowHeight + 'px';
+  }
+});
--- static/benchmarks/js/table-expansion.js
+++ static/benchmarks/js/table-expansion.js
@@ -1,4 +1,93 @@
 $(document).ready(function () {
+    // If this element is present we know we're on the updated view
+    // TODO - we can remove this condition once we're sure we no longer need the old leaderboard view
+    if (document.querySelector('.leaderboard-table-component')) {
+        $('th[data-benchmark]').click(handleToggleBenchmarkDisplay);
+        return;
+    }
+
+    function handleToggleBenchmarkDisplay(event) {
+        const benchmarkCount = event.currentTarget.querySelector('.benchmark-count');
+
+        // the column has no children or the user clicked the average column so do nothing
+        if (!benchmarkCount || (event.type !== "breadcrumb-click" && event.currentTarget.dataset.benchmark === 'average_vision')) { return; }
+
+        // start by hiding everything
+        $('[data-benchmark]').css('display','none');
+        // reshow the element that was actually clicked
+        event.currentTarget.style.display  = '';
+
+        // toggle the open state
+        if ((isClosed = !benchmarkCount.classList.contains('open'))) {
+            event.currentTarget.dataset.benchmark !== 'average_vision' && benchmarkCount.classList.add('open');
+            stepDown(event);
+        } else {
+            benchmarkCount.classList.remove('open');
+            stepUp(event);
+        }
+
+    }
+
+    function stepDown(event) {
+        const benchmark = event.currentTarget.dataset.benchmark;
+        // the `$=` attribute selector e.g. [attr$=val] matches a suffix
+        $(`[data-benchmark$="${benchmark}_v0"],[data-parent="${benchmark}_v0"]`).css('display', '');
+
+        if (event.currentTarget.dataset.benchmark === "average_vision") {
+             $('[data-parent="None"]').css('display', '');
+        }
+        setBreadCrumbs($(event.currentTarget));
+    }
+
+    function stepUp(event) {
+        const benchmark = event.currentTarget.dataset.parent;
+        // special case this so returning back to the main view also expands neural and behavioral columns
+        if (event.currentTarget.dataset.depth === '0' || event.currentTarget.dataset.parent === 'average_vision_v0') {
+            $('[data-parent="None"],[data-parent="average_vision_v0"]').css('display', '');
+        } else {
+            $(`[data-benchmark="${benchmark.replace('_v0', '')}"],[data-benchmark$="${benchmark}"],[data-parent="${benchmark}"]`).css('display', '');
+        }
+        setBreadCrumbs($(`[data-benchmark="${benchmark.replace('_v0', '')}"]`))
+    }
+
+    function setBreadCrumbs(target) {
+        if (target.data() && (target.data().benchmark === "average_vision")) {
+            $('.leaderboard-breadcrumb').html('');
+            return;
+        }
+
+        let breadcrumb = `<span class="breadcrumb-link cursor--pointer">${target.data().benchmark}</span>`;
+
+        if (target.data().benchmark === "engineering_vision") {
+            breadcrumb = `<span class="breadcrumb-link cursor--pointer">average_vision</span> > ` + breadcrumb;
+        }
+        while (target.data().parent !== 'None') {
+            target = $(`[data-benchmark="${target.data().parent.replace('_v0', '')}"]`);
+            breadcrumb = `<span class="breadcrumb-link cursor--pointer">${target.data().benchmark}</span> > ` + breadcrumb;
+
+            if (target.data().benchmark === "engineering_vision") {
+                breadcrumb = `<span class="breadcrumb-link cursor--pointer">average_vision</span> > ` + breadcrumb;
+            }
+
+
+        }
+
+
+        $('.leaderboard-breadcrumb').html(breadcrumb);
+
+        $('.breadcrumb-link').click((event) => {
+            // toggle all the open columns back to their closed state
+            $('.benchmark-count').removeClass('open');
+
+            handleToggleBenchmarkDisplay({
+                type: 'breadcrumb-click',
+                currentTarget: $(`[data-benchmark="${event.currentTarget.innerText}"]`)[0]
+            });
+        });
+    }
+
+    // TODO - This can all be removed once we're sure we're ready to rip out the old leaderboard
+
     // This script functions by finding all the elements whose parents the benchmark that is clicked and setting them to
     // visible or invisible. (And doing it recursively for all of its children)
     var coll = document.getElementsByClassName("clicker");
@@ -8,6 +97,7 @@ $(document).ready(function () {
         // This allows the current value of i to be saved to the assigned function.
         assignFunction(coll, i)
     }
+
 
     function recursiveChildren(all_coll) {
         // Performs recursiveChildren on the current benchmark and checking for all of its children and
@@ -80,4 +170,6 @@ $(document).ready(function () {
             }
         }
     }
+
+
 });
