6bb0ebd1644085fb2d3a18e727d3e29a2a477b75
Mike Ferguson <mef8dd@virginia.edu>
2023-12-18 14:48:49 -0500

    changed URLs to be production-ready (removed preview link)

--- benchmarks/urls.py
+++ benchmarks/urls.py
@@ -8,11 +8,8 @@ supported_domains = ["vision", "language"]
 
 non_domain_urls = [
 
-        # landing page (preview mode)
-        path('2023/', user.LandingPage.as_view(), name='landing_page'),
-
-        path('', functools.partial(index, domain="vision"), name='index'),
-        path('/', functools.partial(index, domain="vision"), name='index'),
+        path('',  user.LandingPage.as_view(), name='landing_page'),
+        path('/', user.LandingPage.as_view(), name='landing_page'),
 
         # user
         path('competition/', competition.view, name='competition'),
