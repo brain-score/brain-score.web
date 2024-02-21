104ee75634e1617f24871a21ac7555b810847ef1
Sam Winebrake <85908068+samwinebrake@users.noreply.github.com>
2024-02-21 13:46:20 -0500

    refix :=

--- benchmarks/views/user.py
+++ benchmarks/views/user.py
@@ -349,8 +349,9 @@ def extract_identifiers(zip_ref):
             with zip_ref.open(file_info) as file:
                 # extract identifier pattern matches
                 for line in TextIOWrapper(file, encoding='utf-8'):
-                    line_code = line.split('#', 1)[0].strip()
-                    if pattern := registry_patterns.get(plugin):
+                    line_code = line.split('#', 1)[0].strip()  # ignore both inline and own line comments
+                    pattern = registry_patterns.get(plugin)
+                    if pattern:
                         matches = pattern.findall(line_code)
                         identifiers[plugin].update(matches)
 
