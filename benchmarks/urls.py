ce78a67795bfc3ca5bd8826e6d10bdc2fe3dfe2f
Michael Ferguson <mferg@mit.edu>
2024-03-13 13:13:59 -0400

    Troubleshooting page (#257)
    * initial commit of troubleshooting page
    
    * Martin suggestion commit

--- benchmarks/urls.py
+++ benchmarks/urls.py
@@ -39,7 +39,8 @@ non_domain_urls = [
         path('tutorial/deepdive_1', user.Tutorial.as_view(tutorial_type="_deepdive_1"), name='tutorial-deepdive-1'),
         path('tutorial/deepdive_2', user.Tutorial.as_view(tutorial_type="_deepdive_2"), name='tutorial-deepdive-2'),
         path('tutorial/deepdive_3', user.Tutorial.as_view(tutorial_type="_deepdive_3"), name='tutorial-deepdive-3'),
-        path('tutorial/deepdive_3', user.Tutorial.as_view(tutorial_type="_deepdive_4"), name='tutorial-deepdive-4'),
+        path('tutorial/troubleshooting', user.Tutorial.as_view(tutorial_type="_troubleshooting"), name='tutorial-troubleshooting'),
+
 
 
         # need navbar links when on /profile. Default to vision.
