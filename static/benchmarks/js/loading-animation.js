ab12bb827b48ae32659b25ac8ed14a6b9e73c7af
Kartik Pradeepan <kartikspradeepan@gmail.com>
2025-08-14 10:23:25 -0400

    Revert "loading on leaderboard"
    This reverts commit e398bdf8c74cca587b9f7df1928d5c5d1d4ae336.

--- static/benchmarks/js/loading-animation.js
+++ static/benchmarks/js/loading-animation.js
@@ -32,7 +32,7 @@ document.addEventListener('click', function (e) {
 
 
 // keep spinner up for a minimum duration
-const SPINNER_MIN_MS = 0;
+const SPINNER_MIN_MS = 5000;
 function hideSpinnerDelayed(ms = SPINNER_MIN_MS) {
   setTimeout(() => LoadingSpinner.hide(), ms);
 }
@@ -117,3 +117,4 @@ document.addEventListener('click', (e) => {
   // We don't preventDefault, so navigation proceeds normally.
   requestAnimationFrame(() => {});
 }, { capture: true });
+
