e24a5e15d09e6898afd3f1d0ca248c6a90d0c557
Kartik Pradeepan <kartikspradeepan@gmail.com>
2025-10-07 15:35:31 -0400

    Modify message

--- benchmarks/views/user.py
+++ benchmarks/views/user.py
@@ -427,11 +427,11 @@ def validate_zip(file: InMemoryUploadedFile) -> Tuple[bool, str]:
         for item in namelist:
             if ' ' in item.filename:
                 return False, f"File '{item.filename}' contains spaces. Please remove spaces from all file names."
-                
+
             # Check for metadata files
             filename = item.filename.lower()
             if filename.endswith('metadata.yaml') or filename.endswith('metadata.yml'):
-                return False, f"We currently do not support metadata files in user submissions. Please remove metadata.yaml/metadata.yml files from your submission."
+                return False, f"We currently do not support user-contributed metadata files. Please remove the metadata.yaml/metadata.yml files from your submission."
 
         root = namelist[0]
         has_plugin, submitted_plugins = plugins_exist(namelist)
