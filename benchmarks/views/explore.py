80505c199abcdb69e3194168274b6ea641459c40
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-02-09 12:02:57 -0500

    Introduce model groups (migration and explore page update)

--- benchmarks/views/explore.py
+++ benchmarks/views/explore.py
@@ -4,14 +4,33 @@ from benchmarks.models import Model, BenchmarkInstance
 
 
 def view(request, domain):
-    models = (Model.objects
-              .filter(domain=domain, public=True)
-              .order_by('name')
-              .values('name', 'id'))
+    base_query = Model.objects.filter(domain=domain, public=True)
+
+    representative_groups = [Model.Group.REFERENCE, Model.Group.TOP10_2024, Model.Group.BASE]
+
+    representative_models = (base_query
+                             .filter(group__in=representative_groups)
+                             .order_by('name')
+                             .values('name', 'id'))
+
+    other_models = (base_query
+                    .filter(group__isnull=True)
+                    .order_by('name')
+                    .values('name', 'id'))
+
+    total_model_count = base_query.count()
+
     benchmarks = (BenchmarkInstance.objects
                   .filter(benchmark_type__domain=domain, benchmark_type__visible=True)
                   .order_by('benchmark_type__identifier', '-version')
                   .distinct('benchmark_type__identifier')
                   .values('id', 'benchmark_type__identifier', 'version'))
-    context = {'domain': domain, 'models': models, 'benchmarks': benchmarks}
+
+    context = {
+        'domain': domain,
+        'representative_models': representative_models,
+        'other_models': other_models,
+        'total_model_count': total_model_count,
+        'benchmarks': benchmarks,
+    }
     return render(request, 'benchmarks/explore.html', context)
