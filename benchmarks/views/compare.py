12cbc9811f014a2b035057cdad700df2c73f65f4
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-03-05 13:59:48 -0500

    Add Model Compare Tab (#509)
    * compare tab for model stats
    
    * styling changes
    
    * default model in compare tab
    
    * address all of Martin's comments

--- benchmarks/views/compare.py
+++ benchmarks/views/compare.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
-from django.views.decorators.cache import cache_page
+import json
 
+from django.shortcuts import render
 
 from .index import get_context
+from .compare_models import (
+    _build_benchmark_domain_map,
+    _build_model_metadata,
+    _build_benchmark_url_map,
+)
+
 
 def view(request, domain: str):
     context = get_context(show_public=True, domain=domain)
-
+    benchmark_domain_map = _build_benchmark_domain_map(context["benchmarks"])
+    context["benchmark_domain_map"] = json.dumps(benchmark_domain_map)
+    context["model_metadata"] = json.dumps(
+        _build_model_metadata(context["models"], domain)
+    )
+    context["benchmark_url_map"] = json.dumps(
+        _build_benchmark_url_map(context["benchmarks"], domain)
+    )
     return render(request, 'benchmarks/compare.html', context)
