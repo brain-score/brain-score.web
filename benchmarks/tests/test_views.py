e5ec9e85359ac295b42cb5111101d7424ca745ef
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-08-05 11:08:52 -0400

    feat(ui): align application shell with landing page

--- benchmarks/tests/test_views.py
+++ benchmarks/tests/test_views.py
@@ -81,6 +81,7 @@ class TestWebsitePages(BaseTestCase):
         """Test the compare page loads"""
         response = self.client.get('/vision/compare/')
         self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<body class="app-shell-page">', response.content)
 
     def test_tutorials_home(self):
         """Test the tutorials home page loads"""
